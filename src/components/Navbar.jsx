8574c2c49e3ddb2e7aa9ebbf4f9b7edad4e952f4
amalkrishna0123 <amalikka0@gmail.com>
2024-08-03 19:37:37 +0530

    Navbar Activated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,15 @@ import React, { useState, useEffect } from 'react';
 const Navbar = () => {
 
     const [menuOpen, setMenuOpen] = useState(false);
+    const [active, setActive] = useState(false)
+
+    useEffect(() => {
+        const scrollActive = () => {
+            setActive(window.scrollY > 20);
+        };
+        window.addEventListener("scroll", scrollActive);
+        return () => window.removeEventListener('scroll', scrollActive);
+    }, []);
 
     const menuOPenHandler = () => {
         setMenuOpen(!menuOpen)
@@ -14,29 +23,29 @@ const Navbar = () => {
 
   return (
 
-      <header className='GlassNavbar py-2 rounded-b-3xl bg-[#ffffff42] z-50'>
-        <div className=" z-50">
-            {menuOpen && <MobileNavbar setMenuOpen = {setMenuOpen}/>}
+    <header className={`GlassNavbar fixed top-0 left-0 w-full py-2 z-[999] transition-all duration-300 ${active ? ' shadow-lg' : 'bg-[#ffffff42]'}`}>
+    <div className="z-50">
+        {menuOpen && <MobileNavbar setMenuOpen={setMenuOpen} />}
+    </div>
+    <nav className='flex justify-between items-center section z-50'>
+        <div>
+            <img src={logo} className='w-[150px] drop-shadow-sm' alt="" />
         </div>
-        <nav className=' flex justify-between items-center section z-50'>
-            <div>
-                <img src={logo} className=' w-[150px] drop-shadow-sm' alt="" />
-            </div>
-            <div className=' hidden md:flex'>
-                <ul className=" flex justify-center gap-5 items-center font-semibold text-[#2d3eff]">
-                    <li className=" cursor-pointer NavbarFont">Home</li>
-                    <li className=" cursor-pointer NavbarFont">About</li>
-                    <li className=" cursor-pointer NavbarFont">Software</li>
-                    <li className=" cursor-pointer NavbarFont">Services</li>
-                    <li className=" cursor-pointer NavbarFont">Clients</li>
-                    <li className=" cursor-pointer NavbarFont">Contact</li>
-                </ul>
-            </div>
-            <div className=" md:hidden">
-                <CgMenuRight className=' text-2xl font-bold text-[#0d00ff] cursor-pointer drop-shadow-sm' onClick={setMenuOpen}/>
-            </div>
-        </nav>
-      </header>
+        <div className='hidden md:flex'>
+            <ul className="flex justify-center gap-5 items-center font-semibold text-[#2d3eff]">
+                <li className="cursor-pointer NavbarFont">Home</li>
+                <li className="cursor-pointer NavbarFont">About</li>
+                <li className="cursor-pointer NavbarFont">Software</li>
+                <li className="cursor-pointer NavbarFont">Services</li>
+                <li className="cursor-pointer NavbarFont">Clients</li>
+                <li className="cursor-pointer NavbarFont">Contact</li>
+            </ul>
+        </div>
+        <div className="md:hidden">
+            <CgMenuRight className='text-2xl font-bold text-[#0d00ff] cursor-pointer drop-shadow-sm' onClick={() => setMenuOpen(true)} />
+        </div>
+    </nav>
+</header>
   )
 }
 
